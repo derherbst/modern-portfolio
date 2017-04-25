792ff4da06e6928b519cd23496af2410c367bcfd
Georgy Gerbst <gerbst.g@gmail.com>
2017-04-26 00:02:34 +0300

    Завершил верстку проекта. Добавил иконочный шрифт, блок features сделал на flexbox

--- gruntfile.js
+++ gruntfile.js
@@ -5,10 +5,21 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks("grunt-browser-sync");
   grunt.loadNpmTasks("grunt-contrib-watch");
   grunt.loadNpmTasks("grunt-contrib-concat");
-  grunt.loadNpmTasks("grunt-autoprefixer");
   grunt.loadNpmTasks("grunt-contrib-less");
+  grunt.loadNpmTasks("grunt-autoprefixer");
+  grunt.loadNpmTasks("grunt-tinyimg");
   
   grunt.initConfig({
+    autoprefixer: {
+      options: {
+        browsers: ['last 2 versions', 'ie 9'] 
+      },
+      my_target: {
+        src: 'css/style.css',
+        dest: 'css/style.css'
+      }
+    },
+    
     less: {
       style: {
         files: {
@@ -37,15 +48,32 @@ module.exports = function(grunt) {
     watch: {
       style: {
         files: ["less/**/*.less"],
-        tasks: ["less"],
+        tasks: ["less", "autoprefixer"],
         options: {
           spawn: false
         }
       }
-    }
+    },
     
+    tinyimg: {
+      static: {
+        files: { 
+          'tmp/img.png': 'img/img.png',
+          'tmp/img.jpg': 'img/img.jpg',
+          'tmp/img.svg': 'img/img.svg'
+        }
+      },
+      dynamic: {
+        files: [{
+          expand: true,
+          cwd: 'img/', 
+          src: ['**/*.{png,jpg,svg}'],
+          dest: 'tmp/'
+        }]
+      }
+    }
   });
   
-  grunt.registerTask("default", ["less", "browserSync", "watch"]);
+  grunt.registerTask("default", ["less", "browserSync", "watch", "autoprefixer"]);
   
 };
